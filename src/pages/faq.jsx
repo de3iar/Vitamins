e50c1b84e1eccb78c63698d6ef186f28887d6bf1
de3iar <3408978042@qq.com>
2023-02-07 04:57:39 +0800

    0.8.1b
    Add FAQ Section "Can not connect to mobile devices."

--- src/pages/faq.jsx
+++ src/pages/faq.jsx
@@ -10,6 +10,29 @@ export default function GetStarted() {
     return <article className="prose bg-base-100 m-20 h-fit">
 
         <Headings h="1" id="" class="" text={i18n.t(lp + 'title')}></Headings>
+        <Headings h="2" id="wifiConn" class="" text={i18n.t(lp + 't_wifiConn')}></Headings>
+        <Headings h="3" id="wireLessConn" class="" text={i18n.t(lp + 't_wireLessConn')}></Headings>
+        <div className=" space-y-8">
+            <ul><span className="text-lg font-bold text-accent ">{i18n.t(lp + 'ul_phoneShowsConnected')}:</span>
+                <li>{i18n.t(lp + 'li_runInAdmin')}</li>
+                <li>{i18n.t(lp + 'li_iosLocalNet')}</li>
+            </ul>
+
+            <ul><span className="text-lg font-bold text-accent">{i18n.t(lp + 'ul_phoneNotConnected')}:</span>
+                <li>{i18n.t(lp + 'li_connToSameRouter')}</li>
+                <li>{i18n.t(lp + 'li_showIp')}</li>
+
+                <img src={imgPath + "faq_compareIp.png"} className="max-w-2xl" alt="" />
+                <p>{i18n.t(lp + 'p_compareIP')}</p>
+                <p>{i18n.t(lp + 'p_traDirectConn')}</p>
+            </ul>
+        </div>
+
+
+
+
+
+
         <Headings h="2" id="vtslag" class="" text={i18n.t(lp + 't_vtslag')}></Headings>
         <li>VBridger v1.07(30/11/2022)
             <video src={videoPath + "faq_jitter_vb.mp4"} controls muted className="w-3/6"></video>
